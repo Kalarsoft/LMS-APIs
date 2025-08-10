914f693b1a4b441ebb54368aabc298c5fbb86acb
Nicholas Kalar <nick@kalar.codes>
2025-08-10 16:21:15 -0400

    Added ContentNegotiation and JSON

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,12 +1,21 @@
 package codes.kalar
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            isLenient = true
+        })
+    }
     configureHTTP()
     configureSecurity()
     configureSerialization()
